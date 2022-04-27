1bcb68c173805a166b7279c092b35fd1e266f622
Park Jin Gon <bennp3038@gmail.com>
2022-04-27 23:54:54 +0900

    HTTP 요청 파라미터 -@ModelAttribute

--- springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -56,16 +58,15 @@ public class RequestParamController {
 	}
 
 	/**
-	 * @RequestParam.required
-	 * /request-param -> username이 없으므로 예외
-	 *
+	 * @RequestParam.required /request-param -> username이 없으므로 예외
+	 * <p>
 	 * 주의!
 	 * /request-param?username= -> 빈문자로 통과
-	 *
+	 * <p>
 	 * 주의!
 	 * /request-param
 	 * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함(또는 다음에 나오는
-	defaultValue 사용)
+	 * defaultValue 사용)
 	 */
 	@ResponseBody
 	@RequestMapping("/request-param-required")
@@ -77,9 +78,8 @@ public class RequestParamController {
 	}
 
 	/**
-	 * @RequestParam
-	 * - defaultValue 사용
-	 *
+	 * @RequestParam - defaultValue 사용
+	 * <p>
 	 * 참고: defaultValue는 빈 문자의 경우에도 적용
 	 * /request-param?username=
 	 */
@@ -104,4 +104,31 @@ public class RequestParamController {
 				paramMap.get("age"));
 		return "ok";
 	}
+
+	/**
+	 * @ModelAttribute 사용
+	 * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model을 설명할 때
+	자세히 설명
+	 */
+	@ResponseBody
+	@RequestMapping("/model-attribute-v1")
+	public String modleAttrtibuteV1(@ModelAttribute HelloData helloData) {
+		log.info("username={}, age={}", helloData.getUsername(),
+				helloData.getAge());
+		return "ok";
+	}
+
+	/**
+	 * @ModelAttribute 생략 가능
+	 * String, int 같은 단순 타입 = @RequestParam
+	 * argument resolver 로 지정해둔 타입 외 = @ModelAttribute
+	 */
+	@ResponseBody
+	@RequestMapping("/model-attribute-v2")
+	public String modelAttributeV2(HelloData helloData) {
+		log.info("username={}, age={}", helloData.getUsername(),
+				helloData.getAge());
+		return "ok";
+	}
+
 }
